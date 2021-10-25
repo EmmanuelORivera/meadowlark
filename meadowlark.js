6d509dec8c7c253dbaea769559f600d5ccdcef1f
Emmanuel Ochoa <eorivera95@gmail.com>
2021-10-25 12:37:07 -0600

    Rendering the html for different routes

--- meadowlark.js
+++ meadowlark.js
@@ -9,28 +9,20 @@ app.set("view engine", "handlebars");
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.type("text/plain");
-  res.send("Meadowlark Travel");
-});
+app.get("/", (req, res) => res.render("home"));
 
-app.get("/about", (req, res) => {
-  res.type("text/plain");
-  res.send("About Meadowlark Travel");
-});
+app.get("/about", (req, res) => res.render("about"));
 
 // custom 404 page
 app.use((req, res) => {
-  res.type("text/plain");
   res.status(404);
-  res.send("404 - Not found");
+  res.render("404");
 });
 
 app.use((err, req, res, next) => {
   console.error(err.message);
-  res.type("text/plain");
   res.status(500);
-  res.send("500 - Server Error");
+  res.render("500");
 });
 
 app.listen(port, () =>
